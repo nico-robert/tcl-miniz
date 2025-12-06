83334682e0a021cfd88569118df1318ad4982b0d
nico-robert <nicolas.robert.19000@gmail.com>
2025-12-06 20:01:44 +0100

    Adds `pkgIndex.tcl` file.

---
+++ pkgIndex.tcl
@@ -0,0 +1,6 @@
+# Copyright (c) 2025 Nicolas ROBERT.
+# Distributed under MIT license. Please see LICENSE for details.
+
+package ifneeded miniz 0.1 [list apply {dir {
+    source [file join $dir miniz.tcl]
+}} $dir]
